1ba79c98eaf730bbea701943ee3ac3c141c2ca7c
Pratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-03-21 08:28:39 +0530

    Added Comments

--- Array/Plus_One/plus_one.cpp
+++ Array/Plus_One/plus_one.cpp
@@ -1,3 +1,6 @@
+// Plus One Problem, LeetCode 66
+// Unoptimal Approach
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -13,20 +16,20 @@ void reverse(vector<int> &nums){
 
 vector<int> plusOne(vector<int> &digits){
     long int integer = 0, i=0;
-    while(i<digits.size()){
+    while(i<digits.size()){ // Convert array to an integer
         integer = integer * 10 + digits[i];
         i++;
     }
-    integer++;
-    vector<int> result;
+    integer++; // Increase the integer by once
+    vector<int> result; // Intitialize the result array
     i = 0;
-    while(integer>0){
+    while(integer>0){ // Convert integer back to an array
         int r = integer%10;
         result.push_back(r);
         integer /= 10;
         i++;
     }
-    reverse(result);
+    reverse(result); // Reverse the result for the expected answer
     return result;
 }
 
