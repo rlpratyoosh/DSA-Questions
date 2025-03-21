8f626ac339816b2181703260faf530fccda2568b
Pratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-03-21 08:22:54 +0530

    Practiced again and added Comments

--- Array/Plus_One/plus_one_2.cpp
+++ Array/Plus_One/plus_one_2.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 vector<int> plusOne(vector<int> &digits){
-    for(int i = digits.size()-1; i>=0 ; i--){
-        if(digits[i]!=9){
+    for(int i = digits.size()-1; i>=0; i--){ // Iterate from end to beginning
+        if(digits[i]!=9){ // If the digit is not a 9 then add one to it and return it
             digits[i]++;
             return digits;
         }
-        digits[i] = 0;
+        digits[i] = 0; // In case it is 9 turn it to 0 and iterate to the next number
     }
-    digits.insert(digits.begin(), 1);
+    digits.insert(digits.begin(), 1); // In case all of the numbers were 9, add 1 to the front
     return digits;
 }
 
